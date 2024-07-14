92488db704c4b750ef397321b5b6a8b7db1864ed
pawank53 <pawankumar930456@gmail.com>
2024-07-14 16:12:25 +0530

    Day1 and Day2

---
+++ Day1/Day1.js
@@ -0,0 +1,40 @@
+// Activity 1: Variable Declaration
+// Task 1
+var variableVar=7
+console.log("variableVar:", variableVar);
+// Task 2
+let variableLet="name"
+console.log("variableLet:",variableLet);
+
+// Activity 2: Constant Declaration
+// Task 3
+const variableConst=true
+console.log("variableConst:", variableConst);
+
+// Activity 3: Data Types
+// Task 4
+var numberVariable=10
+var stringVariable="Saturday"
+var booleanVariable=false
+var objectVariable={
+    name:"My name",
+    address:"India"
+}
+var arrayVariable=[1, 2, 3, 4, 5]
+console.log("Type of numberVariable:", typeof(numberVariable));
+console.log("Type of stringVariable:", typeof(stringVariable));
+console.log("Type of booleanVariable:", typeof(booleanVariable));
+console.log("Type of objectVariable:", typeof(objectVariable));
+console.log("Type of arrayVariable:", typeof(arrayVariable));
+
+// Activity 4: Reasssigning Variables
+// Task 4
+let a=20
+a=30
+console.log("a:", a);
+
+// Activity 5: Understanding Const
+// Task 5
+const b=100
+b=200
+console.log("b:", b); 
---
+++ Day2/Day2.js
@@ -0,0 +1,71 @@
+// Activity 1: Arithmetic Operator
+// Task 1: Write a program to add two number and console the log
+const a=10;
+const b=20;
+const c=a+b
+console.log("a+b:", c);
+
+// Task 2: Write a program to substract two number and console the log
+const a1=10;
+const b1=20;
+const c1=a1-b1
+console.log("a1-b1:", c1);
+
+// Task 3: Write a program to multiply two number and console the log
+const a2=10;
+const b2=20;
+const c2=a2*b2
+console.log("a2*b2:", c2);
+
+// Task 4: Write a program to add divide number and console the log
+const a3=10;
+const b3=20;
+const c3=b3/a3
+console.log("b3/a3:", c3);
+
+// Task 5: Write a program to add two number and console the log
+const a4=10;
+const b4=20;
+const c4=a4%b4
+console.log("a4%b4:", c4);
+
+
+// Activity 2: Assignment Operator
+// Task 6
+var a5=10;
+a5+=10
+console.log("a5:", a5);
+
+// Task 7
+var a6=10;
+a6-=5
+console.log("a6:", a6);
+
+// Activity 3, 4: Comparison and Logical Operator
+// Task 8 && 11
+var num1=15
+console.log("num1>a && num1<b: ", num1>a && num1<b);
+
+// Task 9
+var num1=15
+console.log("num1>=a && num1<=b: ", num1>=a && num1<=b);
+
+// Task 10
+var num1=15
+var num2="15"
+console.log("num1 == num1<b:", num1 == num2);
+console.log("num1 === num1<b:", num1 === num2);
+
+// Task 12
+var num1=15
+console.log("num1>a || num1<c4: ", num1>a || num1<c4);
+
+// Task 13
+var num1=15
+console.log("num1>a || num1<c4: ", !(num1>a || num1<c4));
+
+
+// Activity 5: Ternary Operator
+// Task 14
+var n1=13
+console.log(n1%2===0 ? "Even" : "Odd");
